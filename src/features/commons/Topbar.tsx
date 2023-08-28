7783b724046973c6c44fa6b72e0e2ad647b793d6
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2023-08-28 14:43:07 +0000

    Add Navbar

--- src/features/commons/Topbar.tsx
+++ src/features/commons/Topbar.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-import ConnectWallet from "./ConnectWallet";
+import { useState } from "react";
 import fuseLogo from "../../assets/fuselogo.svg";
+import NavMenu from "./NavMenu";
+import NavButton from "./NavButton";
 
+const menuItems = [
+  {
+    title: "Console",
+    selected: false
+  },
+  {
+    title: "Staking",
+    selected: false
+  },
+  {
+    title: " Bridge",
+    selected: true
+  },
+]
 const Topbar = () => {
+  const [isOpen, setOpen] = useState<boolean>(false)
+
   return (
     <nav className="w-full sticky top-0 bg-light-gray/60 backdrop-blur-xl flex justify-center py-6 z-10 h-[10vh]">
       <div className="flex justify-between h-full items-center w-8/9 md:w-9/10" >
@@ -11,7 +28,8 @@ const Topbar = () => {
             <img src={fuseLogo} alt="fuse logo" className="h-8 z-50" />
           </a>
         </span>
-        <ConnectWallet />
+        <NavMenu menuItems={menuItems} isOpen={isOpen} />
+        <NavButton isOpen={isOpen} setOpen={setOpen} />
       </div>
     </nav>
   );
