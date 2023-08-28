0c19c1d7c7d5ca27462138e6ff953ed9945bb4d9
Mayank Mittal <msquare.pvt21@gmail.com>
2023-08-28 19:56:31 +0530

    Fix Yandex Metrika

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import { Web3OnboardProvider } from "@web3-onboard/react";
 import { web3Onboard } from "./utils/web3onboard";
 import { BrowserRouter } from "react-router-dom";
 import ReactGA from "react-ga4";
-import { init } from '@amplitude/analytics-node';
-
+import { init } from "@amplitude/analytics-node";
+import { YMInitializer } from "react-yandex-metrika";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -22,6 +22,9 @@ root.render(
     <BrowserRouter>
       <Web3OnboardProvider web3Onboard={web3Onboard}>
         <Provider store={store}>
+          <YMInitializer
+            accounts={[parseInt(import.meta.env.VITE_YANDEX_METRICA_ID)]}
+          />
           <App />
         </Provider>
       </Web3OnboardProvider>
