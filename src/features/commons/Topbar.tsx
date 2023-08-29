de7d8b0bd1f5ffe3f96702ff0cafac8a0efd7176
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2023-08-29 07:57:05 +0000

    Remove nav menu

--- src/features/commons/Topbar.tsx
+++ src/features/commons/Topbar.tsx
@@ -1,28 +1,7 @@
-import { useState } from "react";
 import fuseLogo from "../../assets/fuselogo.svg";
-import NavMenu from "./NavMenu";
-import NavButton from "./NavButton";
+import ConnectWallet from "./ConnectWallet";
 
-const menuItems = [
-  {
-    title: "Console",
-    link: "#",
-    selected: false
-  },
-  {
-    title: "Staking",
-    link: "https://staking.fuse.io",
-    selected: false
-  },
-  {
-    title: " Bridge",
-    link: "#",
-    selected: true
-  },
-]
 const Topbar = () => {
-  const [isOpen, setOpen] = useState<boolean>(false)
-
   return (
     <nav className="w-full h-20 sticky top-0 bg-light-gray/60 backdrop-blur-xl flex justify-center py-7 z-40 md:py-0 md:h-[68px]">
       <div className="flex justify-between h-full items-center w-8/9 md:w-9/10 max-w-7xl" >
@@ -31,8 +10,7 @@ const Topbar = () => {
             <img src={fuseLogo} alt="fuse logo" className="h-10 md:h-6 z-50" />
           </a>
         </span>
-        <NavMenu menuItems={menuItems} isOpen={isOpen} />
-        <NavButton isOpen={isOpen} setOpen={setOpen} />
+        <ConnectWallet />
       </div>
     </nav>
   );
